bd852b23fb78d1cfa108b72bb7fa659884ab37b5
Administrator <Administrator@smart20114>
2019-08-29 10:10:24 +0900

    요청하고 요청받기전 interceptor 쓰는방법을 알기위해 배우는 중 받은 프로젝트에 주석을 달아서 설명을 적어보자

---
+++ interceptorTest/src/main/java/kr/og/ksmart/InterceptorTestApplication.java
@@ -0,0 +1,13 @@
+package kr.og.ksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorTestApplication.class, args);
+	}
+
+}
